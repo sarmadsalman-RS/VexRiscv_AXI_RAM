740262161624248961f4c414b5d8f2e1f8151988
Sarmad Salman <ssalman@rslpt09.rapidsilicon.local>
2022-10-04 11:02:50 +0500

    added comments and more read instructions

--- VexRiscv_AXI_RAM/bare_metal_stimulus/src/main.c
+++ VexRiscv_AXI_RAM/bare_metal_stimulus/src/main.c
@@ -9,10 +9,14 @@ void irqCallback(int irq){
 
 void miaou(){
 	int read_val;	
+	//writing
 	*(volatile int *) 0x00000010 = 10;
+	//reading
 	read_val = *(volatile char *) 0x00000010;
 	*(volatile int *) 0x00000030 = 5030;
 	read_val = *(volatile char *) 0x00000030;
+	read_val = *(volatile char *) 0x00000010;
 	*(volatile int *) 0x00000010 = 20;
 	read_val = *(volatile char *) 0x00000010;
+	read_val = *(volatile char *) 0x00000030;
 }
